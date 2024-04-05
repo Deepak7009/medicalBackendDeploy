98db54c030675e97512c6a89b81f99a3ebbf09ee
Deepak7009 <deepak7009verma@gmail.com>
2024-04-05 15:01:08 +0530

    gitignore

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-deco
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config();
+const port = 5000;
+
+const dbConnection = require("./config/dbConnection.js");
+const { gets, addUser, getAllUsers } = require("./controller/userController.js");
+const {register, login, forgetPassword, getUserDetails, updateUsername,updateUsernameInDB } = require("./controller/loginController.js");
+const verifyToken = require("./middleware/middleware.js");
+
+app.use(cors());
+app.use(express.json());
+
+dbConnection();
+
+app.post("/register", register);
+app.post("/login", login);
+app.post("/forget-password", forgetPassword);
+
+app.get("/", gets);
+app.post("/user", addUser);
+app.get("/users", getAllUsers);
+
+app.get("/protected", verifyToken, (req, res) => {
+  res.json({ message: "Protected endpoint accessed", user: req.user });
+});
+
+app.get("/details", verifyToken, getUserDetails);
+
+app.put("/update", verifyToken, updateUsername);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
