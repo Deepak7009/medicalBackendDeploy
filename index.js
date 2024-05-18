f48b10251c55044e8132687c72f4aa947074b6e0
SahilSheorn <sahilks0317@gmail.com>
2024-05-18 22:03:39 +0530

    Updated Code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const dbConnection = require("./config/dbConnection.js");
 const { gets, addUser, getAllUsers } = require("./controller/userController.js");
 const {register, login, forgetPassword, getUserDetails, updateUsername,updateUsernameInDB } = require("./controller/loginController.js");
 const verifyToken = require("./middleware/middleware.js");
+const { addData, getData } = require("./controller/doctorController");
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +22,9 @@ app.post("/forget-password", forgetPassword);
 app.get("/", gets);
 app.post("/user", addUser);
 app.get("/users", getAllUsers);
+
+app.post("/doctor", addData)
+app.get("/doctor", getData);
 
 app.get("/protected", verifyToken, (req, res) => {
   res.json({ message: "Protected endpoint accessed", user: req.user });
